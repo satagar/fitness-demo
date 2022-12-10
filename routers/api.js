eca7a18a67e6b10e9f40b800dca5bace4fb5d045
samiksengupta <samiksengupta@hotmail.com>
2022-12-10 22:27:33 +0530

    added auth and health log controller along with middleware and routes

--- routers/api.js
+++ routers/api.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const authController = require('../controllers/auth.controller');
+const healthLogController = require('../controllers/health-log.controller');
+const { authenticate, authorize, authorizeRoles } = require('../middlewares/auth');
+const validator = require('../middlewares/validators');
 
 const apiRouter = express.Router();
 const apiRouterSecure = express.Router();
@@ -8,6 +12,22 @@ apiRouter.get('/', (req, res) => {
         message: 'You have reached the API service successfully!'
     });
 });
+
+apiRouter.route('/register').post(validator.authRegister, authController.register);
+apiRouter.route('/login').post(validator.authLogin, authController.login);
+apiRouter.route('/logout').post(authController.logout);
+apiRouter.route('/refresh').post(validator.authRefresh, authController.refresh);
+
+apiRouterSecure.use(authenticate);
+
+apiRouterSecure.route('/logs')
+    .get(authorize, healthLogController.index)
+    .post(authorize, validator.healthLogCreate, healthLogController.create);
+
+apiRouterSecure.route('/students/:id')
+    .get(authorize, healthLogController.read)
+    .put(authorize, validator.healthLogUpdate, healthLogController.update)
+    .delete(authorize, healthLogController.destroy);
 
 module.exports = {
     apiRouter: apiRouter, 
